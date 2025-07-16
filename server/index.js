2487b352f13da0454bf493ef5d6db4234fdbba62
Jose Blanco <joseblanco0430906@gmail.com>
2025-07-16 00:52:23 -0400

    Testing Both Node.JS Backend & React.JS Frontend

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// server/index.js
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
