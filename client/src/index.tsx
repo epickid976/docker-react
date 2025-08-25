99237a870cafc1939a1bfe8315554f260ada4136
Jose Blanco <joseblanco0430906@gmail.com>
2025-08-24 23:06:32 -0400

    Commit Fixes Again

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import './App.css';
 import reportWebVitals from "./reportWebVitals";
 
 import { AuthProvider } from "./auth/AuthContext";
